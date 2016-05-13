77f3e4a44a63cf9a36e8ef75d1a8a9c6ef77c1a9
Osamu TONOMORI <osamingo@users.noreply.github.com>
2016-05-13 19:36:16 +0900

    Edit tests (#2)

--- gitio_test.go
+++ gitio_test.go
@@ -15,27 +15,36 @@ func TestGenerateShortURL(t *testing.T) {
 
 	base := "https://git.io"
 
+	c := "gitio-cmd"
 	u, err := url.ParseRequestURI("https://github.com/osamingo/gitio")
 	require.NoError(t, err)
 
 	defer gock.Off()
-	gock.New(base).Post("").ReplyError(errors.New("dummy error"))
+	gock.New(base).
+		Post("").
+		ReplyError(errors.New("dummy error"))
 
-	_, err = GenerateShortURL(u, "")
+	_, err = GenerateShortURL(u, c)
 	require.Error(t, err)
 	assert.EqualError(t, err, "Post https://git.io: dummy error")
 
 	gock.Flush()
-	gock.New(base).Post("").Reply(http.StatusPreconditionFailed).BodyString("dummy body")
+	gock.New(base).
+		Post("").
+		Reply(http.StatusPreconditionFailed).
+		BodyString("dummy body")
 
-	_, err = GenerateShortURL(u, "")
+	_, err = GenerateShortURL(u, c)
 	require.Error(t, err)
 	assert.EqualError(t, err, "invalid http status code\nstatusCode: 412\nmessage: dummy body")
 
 	gock.Flush()
-	gock.New(base).Post("").Reply(http.StatusCreated).SetHeader("location", "https://git.io/gitio-cmd")
+	gock.New(base).
+		Post("").Reply(http.StatusCreated).
+		SetHeader("location", "https://git.io/gitio-cmd").
+		BodyString("https://github.com/osamingo/gitio")
 
-	r, err := GenerateShortURL(u, "")
+	r, err := GenerateShortURL(u, c)
 	require.NoError(t, err)
-	assert.Equal(t, "https://git.io/gitio-cmd", r)
+	assert.Equal(t, base+"/"+c, r)
 }
